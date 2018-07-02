178ec964f53b93b8e702f4f4422ee60fec005977
sheng.xu <sheng.xu@ele.me>
2018-07-02 12:50:10 +0800

    load

---
+++ Chapter_1 Logistic Regression/load_test.py
@@ -0,0 +1,37 @@
+# coding:UTF-8
+'''
+Date:20160901
+@author: zhaozhiyong
+'''
+import numpy as np
+
+def load_data(file_name):
+    '''导入训练数据
+    input:  file_name(string)训练数据的位置
+    output: feature_data(mat)特征
+            label_data(mat)标签
+    '''
+    f = open(file_name)  # 打开文件
+    feature_data = []
+    label_data = []
+    for line in f.readlines():
+        feature_tmp = []
+        lable_tmp = []
+        lines = line.strip().split("\t")
+        feature_tmp.append(1)  # 偏置项
+        for i in xrange(len(lines) - 1):
+            feature_tmp.append(float(lines[i]))
+        lable_tmp.append(float(lines[-1]))
+        
+        feature_data.append(feature_tmp)
+        label_data.append(lable_tmp)
+    f.close()  # 关闭文件
+    print feature_data
+    return np.mat(feature_data), np.mat(label_data)
+
+if __name__ == "__main__":
+    # 1、导入训练数据
+    print "---------- 1.load data ------------"
+    feature, label = load_data("data.txt")
+    # 2、训练LR模型
+    print "---------- 2.training ------------"
